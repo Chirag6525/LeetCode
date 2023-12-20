176a8e32127294c2a63d1d60a38ad32f0551e556
Chirag Kumar <chiragkumar77777@gmail.com>
2023-12-20 17:09:49 +0530

    Medium

---
+++ Binary_Search/Search_rotated_sorted.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target&&nums[mid]>=target){
+                    high = mid -1;
+                }
+                else {
+                    low = mid +1;
+                }
+            }
+            else{
+                if(nums[high]>=target&&nums[mid]<=target){
+                    low = mid +1;
+                }
+                else{
+                    high = mid -1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+int main(){
+    Solution s;
+    int target = 0;
+    vector<int>v = {4,5,6,7,0,1,2};
+    cout << s.search(v,target);
+    return 0;
+}
