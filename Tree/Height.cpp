f23e61396a55ba8587bbada1fa7c3d6bf6aab7c5
Chirag Kumar <chiragkumar77777@gmail.com>
2024-03-06 21:12:47 +0530

    Easy

---
+++ Tree/Height.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        int x= 0,y =0;
+        x = maxDepth(root->left);
+        y = maxDepth(root->right);
+        return x>y?x+1:y+1;
+    }
+};
