8d628df2f69cbef2c69122c2f99e4a5aecd3035e
Chirag Kumar <chiragkumar77777@gmail.com>
2024-03-28 22:57:08 +0530

    Medium

---
+++ Graphs/No_Of_Provinces.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void traverse(int node,vector<int>&visited,vector<vector<int>>&isConnected){
+        int n = isConnected.size();
+        for(int i=0;i<n;i++){
+            if(isConnected[node][i] && !visited[i]){
+                visited[i] = 1;
+                traverse(i,visited,isConnected);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int>visited(n,0);
+        int province =0;
+        for(int i= 0;i<n;i++){
+            if(visited[i]==0){
+                province++;
+            }
+            traverse(i,visited,isConnected);
+        }
+        return province;
+    }
+};
