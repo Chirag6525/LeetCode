b41012022205c30d658fc7232253314dd585af88
Chirag Kumar <chiragkumar77777@gmail.com>
2024-03-31 15:14:43 +0530

    Hard

--- BinaryTree/Maximum_tree_path.cpp
+++ BinaryTree/Maximum_tree_path.cpp
@@ -1,16 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include<bits/stdc++.h>
+using namespace std;
+
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
+    TreeNode * newNode(int data) {
+        TreeNode *n = new TreeNode;
+        n -> val = data;
+        n -> left = NULL;
+        n -> right = NULL;
+        return n;
+    }
     int pathSum(TreeNode* root,int &maxi){
         if(root==NULL) return 0;
         int x = max(0,pathSum(root->left,maxi));
@@ -25,3 +32,11 @@ public:
         return maxi;
     }
 };
+int main(){
+    Solution s;
+    TreeNode *root = s.newNode(-10);
+    root->left = s.newNode(9);
+    root->right = s.newNode(20);
+    cout << "Maximum path: "<<s.maxPathSum(root);
+    return 0;
+}
