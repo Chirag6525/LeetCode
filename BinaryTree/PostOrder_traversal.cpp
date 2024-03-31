3f73676143d8dd6f895606d6d4bea3495822c519
Chirag Kumar <chiragkumar77777@gmail.com>
2024-03-31 15:07:41 +0530

    Easy

--- BinaryTree/PostOrder_traversal.cpp
+++ BinaryTree/PostOrder_traversal.cpp
@@ -1,18 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
 class Solution {
 public:
+    TreeNode * newNode(int data) {
+        TreeNode *n = new TreeNode;
+        n -> val = data;
+        n -> left = NULL;
+        n -> right = NULL;
+
+        return n;
+    }
     void postorder(TreeNode* root,vector<int>&PostOrder){
         if(root==NULL) return;
         postorder(root->left,PostOrder);
@@ -25,3 +32,14 @@ public:
         return PostOrder;
     }
 };
+int main(){
+    Solution s;
+    TreeNode *root = s.newNode(30);
+    root->left = s.newNode(10);
+    root->right = s.newNode(40);
+    vector<int>result = s.postorderTraversal(root);
+    for(auto it: result){
+        cout << it << " ";
+    }
+    return 0;
+}
