679c7de03e344c3f21e645311c277959d00fba6b
Chirag Kumar <chiragkumar77777@gmail.com>
2024-03-31 15:20:39 +0530

    Easy

--- BinaryTree/InOrder_traversal.cpp
+++ BinaryTree/InOrder_traversal.cpp
@@ -1,18 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
+    TreeNode * newNode(int data) {
+        TreeNode *n = new TreeNode;
+        n -> val = data;
+        n -> left = NULL;
+        n -> right = NULL;
+        return n;
+    }
     void inorder(TreeNode* root,vector<int>&InOrder){
         if(root==NULL) return;
         inorder(root->left,InOrder);
@@ -25,3 +30,14 @@ public:
         return InOrder;
     }
 };
+int main(){
+    Solution s;
+    TreeNode *root = s.newNode(30);
+    root->left = s.newNode(10);
+    root->right = s.newNode(40);
+    vector<int>result = s.inorderTraversal(root);
+    for(auto it: result){
+        cout << it << " ";
+    }
+    return 0;
+}
